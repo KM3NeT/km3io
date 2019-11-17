e7eb425b8f4b24006186019317b58f41282a6420
zineb aly <aly.zineb.az@gmail.com>
2019-11-17 20:19:08 +0100

    adapted AanetReader format to pep8

--- km3io/aanet.py
+++ km3io/aanet.py
@@ -1,15 +1,16 @@
 import uproot
 
+
 class AanetReader:
     """Reader for one Aanet ROOT file"""
     def __init__(self, file_path):
         """ AanetReader class is a Aanet ROOT file wrapper
-        
+
         Parameters
         ----------
         file_path : path-like object
-            Path to the file of interest. It can be a str or any python path-like object that points 
-            to the file of ineterst.
+            Path to the file of interest. It can be a str or any python
+            path-like object that points to the file of ineterst.
         """
         self.file_path = file_path
         self.data = uproot.open(self.file_path)['E']
@@ -18,52 +19,65 @@ class AanetReader:
         self._hits_keys = None
         self._tracks_keys = None
 
-
     def __getitem__(self, key):
-        """reads data stored in the branch of interest in an event tree. 
-        
+        """reads data stored in the branch of interest in an event tree.
+
         Parameters
         ----------
         key : str
             name of the branch of interest in event data.
-        
+
         Returns
         -------
         lazyarray
-            Lazyarray of all data stored in the branch of interest. A lazyarray is an array-like
-            object that reads data on demand. Here, only the first and last chunks of data are 
-            read in memory, and not all data in the array. The output can be used
-            with all `Numpy's universal functions <https://docs.scipy.org/doc/numpy/reference/ufuncs.html>`.
-        
+            Lazyarray of all data stored in the branch of interest. A lazyarray
+            is an array-like object that reads data on demand. Here, only the
+            first and last chunks of data are read in memory, and not all data
+            in the array. The output can be used with all `Numpy's universal
+            functions <https://docs.scipy.org/doc/numpy/reference/ufuncs.html>`
+            .
+
         Raises
         ------
         KeyEroor
-            Some branches in an Aanet file structure are "fake branches" and do not contain data. Therefore,
-            the keys corresponding to these fake branches are not read. 
+            Some branches in an Aanet file structure are "fake branches" and do
+            not contain data. Therefore, the keys corresponding to these fake
+            branches are not read.
         """
         if key not in self.keys() and not isinstance(key, int):
             raise KeyError(f"'{key}' is not a valid key or is a fake branch.")
         return self.lazy_data[key]
 
     def __len__(self):
-
         return len(self.lazy_data)
 
     def __repr__(self):
-        return '\n'.join([f"Number of events: {self.__len__()}",
-                          "Events keys are:\n\t" + '\n\t'.join(self.events_keys),
-                          "Hits keys are:\n\t" + '\n\t'.join(self.hits_keys),
-                          "Tracks keys are:\n\t" + '\n\t'.join(self.tracks_keys)])
+        return '\n'.join([
+            f"Number of events: {self.__len__()}",
+            "Events keys are:\n\t" + '\n\t'.join(self.events_keys),
+            "Hits keys are:\n\t" + '\n\t'.join(self.hits_keys),
+            "Tracks keys are:\n\t" + '\n\t'.join(self.tracks_keys)
+        ])
 
     def keys(self):
-        return self.events_keys + self.hits_keys + self.tracks_keys 
+        """constructs a list of all valid keys to be read from an Aanet event file.
+
+        Returns
+        -------
+        list
+            list of all valid keys.
+        """
+        return self.events_keys + self.hits_keys + self.tracks_keys
 
     @property
     def events_keys(self):
         if self._events_keys is None:
-            fake_branches = ['Evt', 'AAObject', 'TObject','t']
+            fake_branches = ['Evt', 'AAObject', 'TObject', 't']
             t_baskets = ['t.fSec', 't.fNanoSec']
-            self._events_keys = [key.decode('utf-8') for key in self.data['Evt'].keys() if key.decode('utf-8') not in fake_branches] + t_baskets
+            self._events_keys = [
+                key.decode('utf-8') for key in self.data['Evt'].keys()
+                if key.decode('utf-8') not in fake_branches
+            ] + t_baskets
         return self._events_keys
 
     @property
@@ -77,5 +91,7 @@ class AanetReader:
     def tracks_keys(self):
         if self._tracks_keys is None:
             tracks_tree = self.data['Evt']['trks']
-            self._tracks_keys = [key.decode('utf8') for key in tracks_tree.keys()]
+            self._tracks_keys = [
+                key.decode('utf8') for key in tracks_tree.keys()
+            ]
         return self._tracks_keys

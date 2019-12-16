ee144d5bda8b583cd4cb7d462b7fe32e30ab2da8
Tamas Gal <tgal@km3net.de>
2019-12-16 23:49:24 +0100

    Add format specifications

---
+++ km3io/trigger.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+KM3NeT Data Definitions v1.0.1
+https://git.km3net.de/common/data-format
+"""
+
+# trigger
+JTRIGGER3DSHOWER = 1
+JTRIGGERMXSHOWER = 2
+JTRIGGER3DMUON = 4
+JTRIGGERNB = 5

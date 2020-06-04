b3ffd2e5f647c8164aac2c7d65ae647377f74549
Tamas Gal <tgal@km3net.de>
2020-06-04 19:46:04 +0200

    Add w2lists

--- km3io/definitions/__init__.py
+++ km3io/definitions/__init__.py
@@ -5,7 +5,11 @@ from .mc_header import data as mc_header
 from .trigger import data as trigger
 from .fitparameters import data as fitparameters
 from .reconstruction import data as reconstruction
+from .w2list_genhen import data as w2list_genhen
+from .w2list_gseagen import data as w2list_gseagen
 
 trigger_idx = {v: k for k, v in trigger.items()}
 fitparameters_idx = {v: k for k, v in fitparameters.items()}
 reconstruction_idx = {v: k for k, v in reconstruction.items()}
+w2list_genhen_idx = {v: k for k, v in w2list_genhen.items()}
+w2list_gseagen_idx = {v: k for k, v in w2list_gseagen.items()}
---
+++ km3io/definitions/w2list_genhen.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+KM3NeT Data Definitions v1.2.1-7-g30d101f
+https://git.km3net.de/common/km3net-dataformat
+"""
+
+# w2list_genhen
+data = {
+    "W2LIST_GENHEN_GLOBAL_GEN_WEIGHT": 0,
+    "W2LIST_GENHEN_EG": 1,
+    "W2LIST_GENHEN_SIG": 2,
+    "W2LIST_GENHEN_COLUMN_DEPTH": 3,
+    "W2LIST_GENHEN_P_EARTH": 4,
+    "W2LIST_GENHEN_REFF": 5,
+    "W2LIST_GENHEN_BX": 7,
+    "W2LIST_GENHEN_BY": 8,
+    "W2LIST_GENHEN_ICHAN": 9,
+    "W2LIST_GENHEN_CC": 10,
+}

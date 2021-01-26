7e6a7bfe74cdcea4ec2c97861b2f356b32ee2b05
Massimiliano Lincetto <mlincetto@km3net.de>
2021-01-26 19:34:50 +0100

    black reformatting

--- km3io/rawacoustic.py
+++ km3io/rawacoustic.py
@@ -1,4 +1,4 @@
-'''
+"""
 This tookit provides an interface to read the raw acoustic binary data tipe as produced by the Acoustic Data Filter (ADF).
 
 The acoustic signals are acquired by the CLB with a 195312.5 Hz sampling frequency (F_S) corresponding to a period of 5.12 microseconds (16 ns * 320).
@@ -9,11 +9,11 @@ The acoustic data filter processes the stream in segments (windows) of given siz
 Typical values are indicated but may change in the future.
 
 When the ADF is set to dump the raw data, the overlapping segment is omitted so the length of the data chunk is FRAME_LENGTH = DAQ_ADF_ANALYSIS_WINDOW_SIZE - DAQ_ADF_ANALYSIS_WINDOW_OVERLAP.
-'''
+"""
 
 import numpy as np
 
-'''
+"""
 Summary description of the raw acoustic data format.
 - 4B: UTC seconds (UNIX timestamp);
 - 4B: number of 16ns cycles;
@@ -26,29 +26,34 @@ As long as FRAME_LENGTH is constant within the same file, this approach will hol
 Note: each file contains data from a single transducer and the DOM or base ID is stored in the filename only. This is not a very good design but here it is tentatively dealt with.
 
 WARNING: data should be in general expected as ordered but may not be contiguous in time.
-'''
+"""
 
-''' sampling frequency of the acoustic stream in the CLB '''
-F_S          = 195312.5
+""" sampling frequency of the acoustic stream in the CLB """
+F_S = 195312.5
+
 
 def get_dtype(FRAME_LENGTH):
-    DATA_TYPE    = np.dtype( [('utc_seconds', np.uint32),
-                       ('16ns_cycles', np.uint32),
-                       ('samples'    , np.uint32),
-                       ('frame', np.float32, FRAME_LENGTH)] )
+    DATA_TYPE = np.dtype(
+        [
+            ("utc_seconds", np.uint32),
+            ("16ns_cycles", np.uint32),
+            ("samples", np.uint32),
+            ("frame", np.float32, FRAME_LENGTH),
+        ]
+    )
     return DATA_TYPE
 
-class RawAcousticReader():
 
-    def __init__(self, filepath, FRAME_LENGTH = 123260):
+class RawAcousticReader:
+    def __init__(self, filepath, FRAME_LENGTH=123260):
 
         self.FRAME_LENGTH = FRAME_LENGTH
         DATA_TYPE = get_dtype(FRAME_LENGTH)
 
         with open(filepath) as acoufile:
             self._data = np.fromfile(acoufile, dtype=DATA_TYPE)
-            ''' extract 8 characters starting from the 5th to get the CLB id '''
-            self._id   = filepath.split('/')[-1][4:1+12] 
+            """ extract 8 characters starting from the 5th to get the CLB id """
+            self._id = filepath.split("/")[-1][4 : 1 + 12]
 
     @property
     def ID(self):
@@ -56,52 +61,44 @@ class RawAcousticReader():
 
     @property
     def pcm(self):
-        ''' Get PCM data concatenating all frames. Data may not be not contiguous.'''
-        return self._data['frame'].flatten()
+        """ Get PCM data concatenating all frames. Data may not be not contiguous."""
+        return self._data["frame"].flatten()
 
     @property
     def timestamps():
-        return self._data['utc_seconds'], self._data['16ns_cycles']
-    
+        return self._data["utc_seconds"], self._data["16ns_cycles"]
+
     @property
     def timebase(self):
-        '''
+        """
         Constructs sequence of times corresponding to each sample.
         For convenience, the time is stored as a double precision float.
         The resolution is not fixed (which is sub-optimal) but should always allow exact representation of the sample time.
-        '''
+        """
         sample_interval = 1 / F_S
-        frame_duration  = self.FRAME_LENGTH * sample_interval
-        time_axis       = np.arange(0, frame_duration, sample_interval)
-        start_frame     = self._data[0]
-        n_samples       = self.FRAME_LENGTH * len(self._data)
-        
+        frame_duration = self.FRAME_LENGTH * sample_interval
+        time_axis = np.arange(0, frame_duration, sample_interval)
+        start_frame = self._data[0]
+        n_samples = self.FRAME_LENGTH * len(self._data)
+
         timebase = np.zeros(n_samples, dtype=np.float64)
-    
+
         for i, frame in enumerate(self._data):
             sample_range = slice(i * self.FRAME_LENGTH, (i + 1) * self.FRAME_LENGTH)
-            timebase[sample_range] = frame['utc_seconds'] + 16e-9 * frame['16ns_cycles'] + time_axis
+            timebase[sample_range] = (
+                frame["utc_seconds"] + 16e-9 * frame["16ns_cycles"] + time_axis
+            )
 
         return timebase
 
-    '''
+    """
     Write as wave, with optional gain.
-    '''
-    def to_wav(self, filepath, gain_dB = 0.0):
+    """
+
+    def to_wav(self, filepath, gain_dB=0.0):
         from scipy.io import wavfile
+
         pcm = self.pcm
         if gain_dB != 0.0:
             pcm *= 10 ** (0.1 * gain_dB)
         wavfile.write(filepath, int(F_S), pcm)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        

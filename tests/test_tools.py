cc6456295af8cfef1b4526191af0cf8ff1ed2127
Jelmer Mulder <jmulder@km3net.de>
2021-04-30 08:24:39 +0000

    Change to logging.debug

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -358,9 +358,9 @@ class TestBestJmuonIndices(unittest.TestCase):
     def test_best_jmuon_indices(self):
         import logging
         m1, m2, m3 = best_jmuon_indices(OFFLINE_FILE.events.tracks)
-        logging.info("m1: {}".format(m1))
-        logging.info("m2: {}".format(m2))
-        logging.info("m3: {}".format(m3))
+        logging.debug("m1: {}".format(m1))
+        logging.debug("m2: {}".format(m2))
+        logging.debug("m3: {}".format(m3))
         #assert len(best_indices) == 10
         #print(best_indices)
 

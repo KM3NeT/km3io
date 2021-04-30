b352a5d92c314be5f849dfd851f9026708cec4e9
Jelmer Mulder <jmulder@km3net.de>
2021-04-30 08:16:58 +0000

    Change logging.info

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -358,9 +358,9 @@ class TestBestJmuonIndices(unittest.TestCase):
     def test_best_jmuon_indices(self):
         import logging
         m1, m2, m3 = best_jmuon_indices(OFFLINE_FILE.events.tracks)
-        logging.warning("m1: ", m1)
-        logging.warning("m2: ", m2)
-        logging.warning("m3: ", m3)
+        logging.info("m1: {}".format(m1))
+        logging.info("m2: {}".format(m2))
+        logging.info("m3: {}".format(m3))
         #assert len(best_indices) == 10
         #print(best_indices)
 

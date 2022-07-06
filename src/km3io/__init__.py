871bbea302f48fb4b993d110f693ee1892819e60
Tamas Gal <himself@tamasgal.com>
2022-07-06 11:22:36 +0200

    Supress OpenMP warning coming from numba

--- src/km3io/__init__.py
+++ src/km3io/__init__.py
@@ -3,6 +3,12 @@ from pkg_resources import get_distribution, DistributionNotFound
 version = get_distribution(__name__).version
 
 import warnings
+import os
+
+# Getting rid of OpenMP warnings, related to Numba
+# See: https://github.com/numba/numba/issues/5275
+# This needs to be done before import numpy
+os.environ["KMP_WARNINGS"] = "off"
 
 with warnings.catch_warnings():
     for warning_category in (FutureWarning, DeprecationWarning):
